1fae2317986ff679bf4cdf1bcad509fe99fd76a7
Madan Gowda S P <madanspgowda@gmail.com>
2026-02-23 06:24:30 +0500

    TodoList

---
+++ src/main/java/com/example/TodoList/Controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.TodoList.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+  @GetMapping("/")
+  public String indexpage(){
+    return "index";
+  }  
+}
---
+++ src/main/java/com/example/TodoList/Entity/TodoEntity.java
@@ -0,0 +1,4 @@
+package com.example.TodoList.Entity;
+
+public class TodoEntity {
+}
